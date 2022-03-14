117c951cf84b2aba61767d3dc1cdeca506a976bc
Mark Standen <mark@markstanden.dev>
2022-03-14 05:47:25 +0000

    changed server engine to netty

--- src/main/kotlin/dev/markstanden/Application.kt
+++ src/main/kotlin/dev/markstanden/Application.kt
@@ -2,11 +2,11 @@ package dev.markstanden
 
 import dev.markstanden.plugins.configureRouting
 import dev.markstanden.plugins.configureSecurity
-import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(CIO, port = System.getenv("PORT")?.toIntOrNull() ?: 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = System.getenv("PORT")?.toIntOrNull() ?: 8080, host = "0.0.0.0") {
         configureSecurity()
         configureRouting()
     }.start(wait = true)
