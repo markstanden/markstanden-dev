479c70efc7edf9d4d8d7c3207d3f76f8a9c998ab
Mark Standen <mark@markstanden.dev>
2022-03-16 05:53:15 +0000

    added root route redirect

--- src/main/kotlin/markstanden/dev/plugins/Routing.kt
+++ src/main/kotlin/markstanden/dev/plugins/Routing.kt
@@ -2,11 +2,15 @@ package markstanden.dev.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
 
     routing {
+        get("/") {
+            call.respondRedirect("pages/home.html")
+        }
         static("/static") { resources("/files") }
         static("/styles") { resources("/files/css") }
         static("/pages") { resources("/files/html") }
