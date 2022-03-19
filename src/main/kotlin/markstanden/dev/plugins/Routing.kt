a35de282b456c9784d300f97ecd9841cda46036e
Mark Standen <mark@markstanden.dev>
2022-03-19 09:56:13 +0000

    added dual static routes

--- src/main/kotlin/markstanden/dev/plugins/Routing.kt
+++ src/main/kotlin/markstanden/dev/plugins/Routing.kt
@@ -4,11 +4,22 @@ import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 import markstanden.dev.routes.homepageRouting
+import java.io.File
 
 fun Application.configureRouting() {
 
     routing {
-        static("static") { resources("files/") }
+        static("/static") {
+            resources("files")
+        }
+
+        static("/static") {
+            staticRootFolder = File("/static")
+            static("assets") {
+                files("css")
+                files("images")
+            }
+        }
         homepageRouting()
     }
 
