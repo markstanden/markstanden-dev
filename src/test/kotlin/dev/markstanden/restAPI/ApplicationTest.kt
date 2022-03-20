ae7074eeac5854493e14dcd724b55c0d129facd8
Mark Standen <mark@markstanden.dev>
2022-03-20 06:48:50 +0000

    removed unnecessary imports from test

--- src/test/kotlin/dev/markstanden/restAPI/ApplicationTest.kt
+++ src/test/kotlin/dev/markstanden/restAPI/ApplicationTest.kt
@@ -1,19 +1,8 @@
 package dev.markstanden.restAPI
 
 import dev.markstanden.restAPI.plugins.configureRouting
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.http.content.*
-import freemarker.cache.*
-import io.ktor.server.freemarker.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import kotlin.test.*
 import io.ktor.server.testing.*
-import markstanden.dev.plugins.*
+import kotlin.test.Test
 
 class ApplicationTest {
     @Test
